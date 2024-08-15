87e01f8d3759063961137418f280169eed2a00af
Dmitry Mottl <dmitry.mottl@gmail.com>
2024-08-15 18:59:18 +0800

    Adds doxygen support

--- lightgbm3-sys/build.rs
+++ lightgbm3-sys/build.rs
@@ -5,6 +5,15 @@ use std::{
     process::Command,
 };
 
+#[derive(Debug)]
+struct DoxygenCallback;
+
+impl bindgen::callbacks::ParseCallbacks for DoxygenCallback {
+    fn process_comment(&self, comment: &str) -> Option<String> {
+        Some(doxygen_rs::transform(comment))
+    }
+}
+
 fn main() {
     let target = env::var("TARGET").unwrap();
     let out_dir = env::var("OUT_DIR").unwrap();
@@ -55,6 +64,7 @@ fn main() {
         .allowlist_file("lightgbm/include/LightGBM/c_api.h")
         .clang_args(&["-x", "c++", "-std=c++11"])
         .clang_arg(format!("-I{}", lgbm_root.join("include").display()))
+        .parse_callbacks(Box::new(DoxygenCallback))
         .generate()
         .expect("Unable to generate bindings");
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
